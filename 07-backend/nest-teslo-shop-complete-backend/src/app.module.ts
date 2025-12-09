0aa6993311470e8032de89a713cca8988f61ef58
Jair Cobos <jaircobos@gmail.com>
2025-12-09 15:05:15 -0500

    app

--- 07-backend/nest-teslo-shop-complete-backend/src/app.module.ts
+++ 07-backend/nest-teslo-shop-complete-backend/src/app.module.ts
@@ -14,44 +14,40 @@ import { MessagesWsModule } from './messages-ws/messages-ws.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
-    
-    isGlobal: true,
-
+   
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
 
     TypeOrmModule.forRoot({
       ssl: process.env.STAGE === 'prod',
       extra: {
-        ssl: process.env.STAGE === 'prod'
-              ? { rejectUnauthorized: false }
-              : null,
+        ssl:
+          process.env.STAGE === 'prod'
+            ? { rejectUnauthorized: false }
+            : null,
       },
       type: 'postgres',
       host: process.env.DB_HOST,
       port: +process.env.DB_PORT,
       database: process.env.DB_NAME,
       username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,      
+      password: process.env.DB_PASSWORD,
       autoLoadEntities: true,
-      synchronize: true,
+      synchronize: true, 
     }),
 
     ServeStaticModule.forRoot({
-      rootPath: join(__dirname,'..','public'), 
+      rootPath: join(__dirname, '..', 'public'),
     }),
 
+
     ProductsModule,
-
     CommonModule,
-
     SeedModule,
-
     FilesModule,
-
     AuthModule,
-
     MessagesWsModule,
-
   ],
 })
 export class AppModule {}
