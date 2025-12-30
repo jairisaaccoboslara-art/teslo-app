ad210dba52f4a8b727a4ebbf055bc7a74b0f0a14
Jair Cobos <jaircobos@gmail.com>
2025-12-30 09:35:07 -0500

    up

--- 07-backend/nest-teslo-shop-complete-backend/src/app.module.ts
+++ 07-backend/nest-teslo-shop-complete-backend/src/app.module.ts
@@ -31,25 +31,20 @@ console.log({
 
     TypeOrmModule.forRoot({
   type: 'postgres',
+  host: process.env.DB_HOST,
+  port: +process.env.DB_PORT,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 
-  // 👉 PRODUCCIÓN (Render)
-  ...(process.env.STAGE === 'prod'
-    ? {
-        url: process.env.DATABASE_URL,
-        ssl: { rejectUnauthorized: false },
-      }
-    : {
-        // 👉 DESARROLLO (Docker local)
-        host: process.env.DB_HOST,
-        port: +process.env.DB_PORT,
-        database: process.env.DB_NAME,
-        username: process.env.DB_USERNAME,
-        password: process.env.DB_PASSWORD,
-      }),
+  ssl: {
+    rejectUnauthorized: false,
+  },
 
   autoLoadEntities: true,
   synchronize: true,
 }),
+
 
 
     ServeStaticModule.forRoot({
